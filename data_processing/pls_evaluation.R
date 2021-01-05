3687d16b6b57b0da961cc4361a12e696fa5ad138
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-01-05 16:16:57 -0700

    PLS evaluation comparisons

--- data_processing/pls_evaluation.R
+++ data_processing/pls_evaluation.R
@@ -1,215 +1,159 @@
 library(doSNOW)
 
-pls_evaluation <- function(train, train_ID, test, test_ID, model, n_comp = 30, resamples = 1000, split = 0.7, threads = 4) {
+pls_evaluation <- function(model, ref, cwt, data_split = data_split, samples_ID, sec_split = 0.7, n_comp_ref = 30, n_comp_cwt = 19, iterations = 20, threads = 12) {
   
-  ###Parallel---------------------------------------
-  cl <- makeCluster(threads)
-  registerDoSNOW(cl)
+  ###Subdatasets for training and testing
+  ref_training <- ref[data_split,]
+  cwt_training <- cwt[data_split,]
+  
+  ref_testing <- ref[-data_split,]
+  cwt_testing <- cwt[-data_split,]
+  
+  lf_training <- samples_ID[data_split,] 
+  lf_testing <- samples_ID[-data_split,]
+  
+  ###Wavelength names
+  wavelength <- colnames(ref)[-1]
   
   ###Progress bar -----------------------------------
-  pb <- txtProgressBar(min = 1, max = resamples, style = 3) 
+  pb <- txtProgressBar(min = 1, max = iterations, style = 3) 
   progress <- function(n) setTxtProgressBar(pb, n)
   opts <- list(progress = progress)
   
+  ###Parallel---------------------------------------
+  cl <- makeCluster(threads)
+  doSNOW::registerDoSNOW(cl)
+  
   ###Model loop-------------------------------
  
-  final <- foreach(i = 1:resamples, .packages= c('base', 'data.table', 'foreach', 'caret', 'pls', 'plsVarSel'), .export= c("performance"), .options.snow = opts) %dopar% {
+  final <- foreach(i = 1:iterations, .packages= c('base', 'data.table', 'foreach', 'caret', 'pls', 'plsVarSel'), .export= c("performance"), .options.snow = opts) %dopar% {
     
-    ###Equal split for traing based on Life forms-------------------------------------
-    data_split <- createDataPartition(train_ID$Life_form, list = FALSE, p = split, times = 1)
+    ###Equal sub_split for training based on Life forms-------------------------------------
+    sub_split <- caret::createDataPartition(lf_training$Life_form, list = FALSE, p = sec_split, times = 1)
     
-    cal_data <- train[data_split, ]
-    
-    val_lianas <- train[train_ID$Life_form == "Liana", ]
-    val_trees <- train[train_ID$Life_form == "Tree", ]
+    #Calibration algorithms
+    cal_ref <- ref_training[sub_split, ]
+    cal_cwt <- cwt_training[sub_split, ]
     
     ### Build PLSR model with training data--------------------------------------------
-    pls_results <- plsr(model, 
-                        ncomp = n_comp, 
+    pls_ref <- plsr(model, 
+                        ncomp = n_comp_ref, 
                         scale = FALSE, 
                         centre = TRUE, 
                         validation= "none", 
-                        data= cal_data, 
+                        data= cal_ref, 
                         method = "oscorespls")
     
+    pls_cwt <- plsr(model, 
+                    ncomp = n_comp_cwt, 
+                    scale = FALSE, 
+                    centre = TRUE, 
+                    validation= "none", 
+                    data= cal_cwt, 
+                    method = "oscorespls")
+    
     ###Store the model arguments
-    name <- paste("", "resample_", i, sep = "")
+    #Create frame
+    name <- paste0("resample_", i)
+    frame <- data.table(Spectra = c("Reflectance", "CWT"), iteration = name)
     
-    frame_coefs <- data.table(name = as.vector(coef(pls_results, ncomp = n_comp, intercept=TRUE)))
-    colnames(frame_coefs) <- name
+    performance_frame <- data.table(Spectra = rep(c(rep("Reflectance", 3), rep("CWT", 3)), 2),
+                                    Model = c(rep("Training", 6), rep("Testing", 6)),
+                                    Life_form = rep(c("All", "Tree", "Liana"), 4),
+                                    iteration = name)
     
-    frame_VIP <- data.table(name = as.vector(VIP(pls_results, n_comp)))
-    colnames(frame_VIP) <- name
-                             
+    #Save coefficients
+    ref_coefs <- as.vector(coef(pls_ref, ncomp = n_comp_ref, intercept=TRUE))
+    cwt_coefs <- as.vector(coef(pls_cwt, ncomp = n_comp_cwt, intercept=TRUE))
+    coef <- rbind(ref_coefs, cwt_coefs)
+    
+    frame_coef <- cbind(frame, coef)
+    colnames(frame_coef) <- c("Spectra", "iteration", "intercept", wavelength)
+    
+    #Save VIP
+    ref_VIP <- as.vector(VIP(pls_ref, opt.comp = n_comp_ref))
+    cwt_VIP <- as.vector(VIP(pls_cwt, opt.comp = n_comp_cwt))
+    VIP <- rbind(ref_VIP, cwt_VIP)
+    
+    frame_VIP <- cbind(frame, VIP)
+    colnames(frame_VIP) <- c("Spectra", "iteration", wavelength)
+    
     ### Estimate variability in the model----------------------------------------------------------------------
-    pred_val <- as.vector(predict(pls_results, 
-                                       newdata= train,
-                                       ncomp = n_comp,
-                                       type= "response")[,,1])
-    pred_val <- 10^pred_val
+    ##Predict values
+    #Training model
+    ref_training_predicted_all <- 10^as.vector(predict(pls_ref, newdata= ref_training, ncomp = n_comp_ref, type= "response")[,,1])
+    ref_training_predicted_trees <- 10^as.vector(predict(pls_ref, newdata= ref_training[lf_training$Life_form == "Tree",], ncomp = n_comp_ref, type= "response")[,,1])
+    ref_training_predicted_lianas <- 10^as.vector(predict(pls_ref, newdata= ref_training[lf_training$Life_form == "Liana",], ncomp = n_comp_ref, type= "response")[,,1])
+      
+    cwt_training_predicted_all <- 10^as.vector(predict(pls_cwt, newdata= cwt_training, ncomp = n_comp_cwt, type= "response")[,,1])
+    cwt_training_predicted_trees <- 10^as.vector(predict(pls_cwt, newdata= cwt_training[lf_training$Life_form == "Tree",], ncomp = n_comp_cwt, type= "response")[,,1])
+    cwt_training_predicted_lianas <- 10^as.vector(predict(pls_cwt, newdata= cwt_training[lf_training$Life_form == "Liana",], ncomp = n_comp_cwt, type= "response")[,,1])    
+      
+    #Testing model
+    ref_testing_predicted_all <- 10^as.vector(predict(pls_ref, newdata= ref_testing, ncomp = n_comp_ref, type= "response")[,,1])
+    ref_testing_predicted_trees <- 10^as.vector(predict(pls_ref, newdata= ref_testing[lf_testing$Life_form == "Tree",], ncomp = n_comp_ref, type= "response")[,,1])
+    ref_testing_predicted_lianas <- 10^as.vector(predict(pls_ref, newdata= ref_testing[lf_testing$Life_form == "Liana",], ncomp = n_comp_ref, type= "response")[,,1])
     
-    pred_val_lianas <- as.vector(predict(pls_results, 
-                                       newdata= val_lianas,
-                                       ncomp = n_comp,
-                                       type= "response")[,,1])
-    pred_val_lianas <- 10^pred_val_lianas
+    cwt_testing_predicted_all <- 10^as.vector(predict(pls_cwt, newdata= cwt_testing, ncomp = n_comp_cwt, type= "response")[,,1])
+    cwt_testing_predicted_trees <- 10^as.vector(predict(pls_cwt, newdata= cwt_testing[lf_testing$Life_form == "Tree",], ncomp = n_comp_cwt, type= "response")[,,1])
+    cwt_testing_predicted_lianas <- 10^as.vector(predict(pls_cwt, newdata= cwt_testing[lf_testing$Life_form == "Liana",], ncomp = n_comp_cwt, type= "response")[,,1])    
     
-    pred_val_trees <- as.vector(predict(pls_results, 
-                                       newdata= val_trees,
-                                       ncomp = n_comp,
-                                       type= "response")[,,1])
-    pred_val_trees <- 10^pred_val_trees
+    ###Evaluate performance
+    #Training
+    ref_training_performance_all <- performance((10^ref_training[[1]]), ref_training_predicted_all)
+    ref_training_performance_trees <- performance((10^ref_training[lf_training$Life_form == "Tree",][[1]]), ref_training_predicted_trees)
+    ref_training_performance_lianas <- performance((10^ref_training[lf_training$Life_form == "Liana",][[1]]), ref_training_predicted_lianas)
     
-    ###Frame to fill
+    cwt_training_performance_all <- performance((10^cwt_training[[1]]), cwt_training_predicted_all)
+    cwt_training_performance_trees <- performance((10^cwt_training[lf_training$Life_form == "Tree",][[1]]), cwt_training_predicted_trees)
+    cwt_training_performance_lianas <- performance((10^cwt_training[lf_training$Life_form == "Liana",][[1]]), cwt_training_predicted_lianas)
     
-    frame_stats <- data.frame(Resample = rep(name, 6),
-                              Process = rep(NA, 6),
-                              Life_form = rep(NA, 6),
-                              Samples = rep(NA, 6),
-                              Rsq = rep(NA, 6),
-                              Bias= rep(NA, 6),
-                              RMSE = rep(NA, 6),
-                              RMSE_P = rep(NA, 6))
+    #Testing model
+    ref_testing_performance_all <- performance((10^ref_testing[[1]]), ref_testing_predicted_all)
+    ref_testing_performance_trees <- performance((10^ref_testing[lf_testing$Life_form == "Tree",][[1]]), ref_testing_predicted_trees)
+    ref_testing_performance_lianas <- performance((10^ref_testing[lf_testing$Life_form == "Liana",][[1]]), ref_testing_predicted_lianas)
     
-    ###All---------------------------
-    # Error statistics
-    trait_val <- 10^train[, 1]
-
-    val_perf <- performance(trait_val, pred_val)
-
-    ### Store results of iteration i
-    frame_stats[1,2] <- "Training"
-    frame_stats[1,3] <- "All"
-    frame_stats[1,4] <- val_perf[1]
-    frame_stats[1,5] <- val_perf[2]
-    frame_stats[1,6] <- val_perf[3]
-    frame_stats[1,7] <- val_perf[4]
-    frame_stats[1,8] <- val_perf[5]
+    cwt_testing_performance_all <- performance((10^cwt_testing[[1]]), cwt_testing_predicted_all)
+    cwt_testing_performance_trees <- performance((10^cwt_testing[lf_testing$Life_form == "Tree",][[1]]), cwt_testing_predicted_trees)
+    cwt_testing_performance_lianas <- performance((10^cwt_testing[lf_testing$Life_form == "Liana",][[1]]), cwt_testing_predicted_lianas)
     
-    ###Lianas------------------------
-    # Error statistics
-    trait_val_lianas <- 10^val_lianas[, 1]
-    val_perf_lianas <- performance(trait_val_lianas, pred_val_lianas)
-
-    ### Store results of iteration i
-    frame_stats[2,2] <- "Training"
-    frame_stats[2,3] <- "Lianas"
-    frame_stats[2,4] <- val_perf_lianas[1]
-    frame_stats[2,5] <- val_perf_lianas[2]
-    frame_stats[2,6] <- val_perf_lianas[3]
-    frame_stats[2,7] <- val_perf_lianas[4]
-    frame_stats[2,8] <- val_perf_lianas[5]
+    ###Collect values of performance
+    performance_merge <- rbind(ref_training_performance_all, ref_training_performance_trees, ref_training_performance_lianas,
+                         cwt_training_performance_all, cwt_training_performance_trees, cwt_training_performance_lianas,
+                         ref_testing_performance_all, ref_testing_performance_trees, ref_testing_performance_lianas,
+                         cwt_testing_performance_all, cwt_testing_performance_trees, cwt_testing_performance_lianas)
     
-    ###Trees-------------------------
-    # Error statistics
-    trait_val_trees <- 10^val_trees[, 1]
-    val_perf_trees <- performance(trait_val_trees, pred_val_trees)
-
-    ### Store results of iteration i
-    frame_stats[3,2] <- "Training"
-    frame_stats[3,3] <- "Trees"
-    frame_stats[3,4] <- val_perf_trees[1]
-    frame_stats[3,5] <- val_perf_trees[2]
-    frame_stats[3,6] <- val_perf_trees[3]
-    frame_stats[3,7] <- val_perf_trees[4]
-    frame_stats[3,8] <- val_perf_trees[5]
+    performance_frame <- cbind(performance_frame, performance_merge)
     
-    ### Predict traits train----------------------------------------------------------------------
-    frame_predict_train <- data.table(name = (pred_val))
-    colnames(frame_predict_train) <- name
+    ###Collect predicted values
+    training_predict <- cbind(frame, rbind(ref_training_predicted_all, cwt_training_predicted_all))
+    colnames(training_predict) <- c("Spectra", "iteration", paste0("Sample_", lf_training$Sample))
     
-    ### Residuals train
-    residuals_train <- round((trait_val) - (pred_val), 5)
-    residuals_train <- data.table(residuals_train = residuals_train)
-    colnames(residuals_train) <- name
+    testing_predict <- cbind(frame, rbind(ref_testing_predicted_all, cwt_testing_predicted_all))
+    colnames(testing_predict) <- c("Spectra", "iteration", paste0("Sample_", lf_testing$Sample))
     
-    ### Estimate variability in the testing model-----------------------------------------------------------------
+    ###Estimated and collect residuals
+    ref_training_residuals <- ref_training_predicted_all - (10^ref_training[[1]])
+    cwt_training_residuals <- cwt_training_predicted_all - (10^cwt_training[[1]])
     
-    is_liana <- test_ID$Life_form == "Liana"
+    ref_testing_residuals <- ref_testing_predicted_all - (10^ref_testing[[1]])
+    cwt_testing_residuals <- cwt_testing_predicted_all - (10^ref_testing[[1]])
     
-    test_lianas <- test[is_liana,]
-    test_trees <- test[!is_liana,]
+    training_residuals <- cbind(frame, rbind(ref_training_residuals, cwt_training_residuals))
+    colnames(training_residuals) <- c("Spectra", "iteration", paste0("Sample_", lf_training$Sample))
     
-    pred_test <- as.vector(predict(pls_results, 
-                                       newdata= test,
-                                       ncomp = n_comp,
-                                       type= "response")[,,1])
-    pred_test <- 10^pred_test
-    
-    pred_test_lianas <- as.vector(predict(pls_results, 
-                                         newdata= test_lianas,
-                                         ncomp = n_comp,
-                                         type= "response")[,,1])
-    pred_test_lianas <- 10^pred_test_lianas
-    
-    pred_test_trees <- as.vector(predict(pls_results, 
-                                        newdata= test_trees,
-                                        ncomp = n_comp,
-                                        type= "response")[,,1])
-    pred_test_trees <- 10^pred_test_trees
-    
-    ###All---------------------------
-    # Error statistics
-    trait_test <- 10^test[, 1]
-    test_perf <- performance(trait_test, pred_test)
-
-    ### Store results of iteration i
-    frame_stats[4,2] <- "Testing"
-    frame_stats[4,3] <- "All"
-    frame_stats[4,4] <- test_perf[1]
-    frame_stats[4,5] <- test_perf[2]
-    frame_stats[4,6] <- test_perf[3]
-    frame_stats[4,7] <- test_perf[4]
-    frame_stats[4,8] <- test_perf[5]
-    
-    ###Lianas------------------------
-    # Error statistics
-    trait_test_lianas <- 10^test_lianas[, 1]
-    test_perf_lianas <- performance(trait_test_lianas, pred_test_lianas)
-    
-    ### Store results of iteration i
-    frame_stats[5,2] <- "Testing"
-    frame_stats[5,3] <- "Lianas"
-    frame_stats[5,4] <- test_perf_lianas[1]
-    frame_stats[5,5] <- test_perf_lianas[2]
-    frame_stats[5,6] <- test_perf_lianas[3]
-    frame_stats[5,7] <- test_perf_lianas[4]
-    frame_stats[5,8] <- test_perf_lianas[5]
-    
-    ###Trees-------------------------
-    # Error statistics
-    trait_test_trees <- 10^test_trees[, 1]
-    test_perf_trees <- performance(trait_test_trees, pred_test_trees)
-    
-    ### Store results of iteration i
-    frame_stats[6,2] <- "Testing"
-    frame_stats[6,3] <- "Trees"
-    frame_stats[6,4] <- test_perf_trees[1]
-    frame_stats[6,5] <- test_perf_trees[2]
-    frame_stats[6,6] <- test_perf_trees[3]
-    frame_stats[6,7] <- test_perf_trees[4]
-    frame_stats[6,8] <- test_perf_trees[5]
-    
-    ### Predict traits----------------------------------------------------------------------
-    
-    frame_predict_test <- data.table(name = pred_test)
-    colnames(frame_predict_test) <- name
-    
-    ### Residuals test
-    residuals_test <- round(trait_test - pred_test, 5)
-    residuals_test <- data.table(residuals_test = residuals_test)
-    colnames(residuals_test) <- name
+    testing_residuals <- cbind(frame, rbind(ref_testing_residuals, cwt_testing_residuals))
+    colnames(testing_residuals) <- c("Spectra", "iteration", paste0("Sample_", lf_testing$Sample))
     
     ###Return list--------------------------------------------------------------------------
     
-    return(list(Coefficients = frame_coefs, 
+    return(list(Coefficients = frame_coef, 
                 VIP = frame_VIP, 
-                Stats = frame_stats, 
-                Predict_train = frame_predict_train,
-                Predict_test = frame_predict_test, 
-                Residuals_train = residuals_train,
-                Residuals_test = residuals_test))
-    
+                Performance = performance_frame, 
+                Predict_training = training_predict,
+                Predict_testing = testing_predict, 
+                Residuals_training = training_residuals,
+                Residuals_testing = testing_residuals))
     }
   
   close(pb)
@@ -234,27 +178,23 @@ combine_custom <- function(final) {
   test_resid <- NULL
   
   for (i in seq(n)) {
-    coe <- cbind(coe, final[[i]]$Coefficients)
-    vip <- cbind(vip, final[[i]]$VIP)
-    stats <- rbind(stats, final[[i]]$Stats)
-    train_pred <- cbind(train_pred, final[[i]]$Predict_train)
-    test_pred <- cbind(test_pred, final[[i]]$Predict_test)
-    train_resid <- cbind(train_resid, final[[i]]$Residuals_train)
-    test_resid <- cbind(test_resid, final[[i]]$Residuals_test)
-    
-  
+    coe <- rbind(coe, final[[i]]$Coefficients)
+    vip <- rbind(vip, final[[i]]$VIP)
+    stats <- rbind(stats, final[[i]]$Performance)
+    train_pred <- rbind(train_pred, final[[i]]$Predict_training)
+    test_pred <- rbind(test_pred, final[[i]]$Predict_testing)
+    train_resid <- rbind(train_resid, final[[i]]$Residuals_training)
+    test_resid <- rbind(test_resid, final[[i]]$Residuals_testing)
   }
 
     return(list(Coefficients = coe, 
                 VIP = vip, 
-                Stats = stats, 
-                Predict_train = train_pred,
-                Predict_test = test_pred, 
-                Residuals_train = train_resid,
-                Residuals_test = test_resid))
+                Performance = stats, 
+                Predict_training = train_pred,
+                Predict_testing = test_pred, 
+                Residuals_training = train_resid,
+                Residuals_testing = test_resid))
 }
-
-
 
 ###Model performance-----------------------------------
 
@@ -263,15 +203,16 @@ performance <- function(observed, predicted) {
   obser <- observed
   pred <- predicted
   
-  rss <- sum((pred - obser) ^ 2)  ## residual sum of squares
+  rss <- sum((obser - pred) ^ 2)  ## residual sum of squares
   tss <- sum((obser - mean(obser))^2)  ## total sum of squares
   
   n <- length(predicted)
   Rsq <- 1 - (rss/tss)
-  Bias <- mean(((pred - obser)/obser))
+  Bias <- mean(((obser - pred)/obser))
   RMSE <- sqrt(mean((obser - pred)^2))
   RMSE_P <- (RMSE/(max(obser)-min(obser)))*100
   
   return(round(c(N = n, Rsq = Rsq, Bias = Bias, RMSE = RMSE, RMSE_P = RMSE_P), 6))
 
 }
+
